2b7674edef516ec9fde593a5ea9d7569096fdf3f
Camille-kyo30 <gnandekouakou710@gmail.com>
2026-04-08 21:22:59 +0000

    Update out.js

--- scripts/cmds/out.js
+++ scripts/cmds/out.js
@@ -1,34 +1,53 @@
+const axios = require("axios");
+const fs = require("fs-extra");
+const request = require("request");
+
 module.exports = {
-  config: {
-    name: "out",
-    aliases: ["leave"],
-    version: "1.0",
-    author: "Christus",
-    countDown: 5,
-    role: 3,
-    shortDescription: {
-      en: "Le bot quitte le groupe",
-    },
-    category: "owner",
-    guide: {
-      en: "{pn} — Faire quitter le bot de ce groupe"
-    }
-  },
+ config: {
+  name: "out",
+  aliases: ["l"],
+  version: "3.0",
+  author: "Camille 🩵",
+  countDown: 5,
+  role: 2,
+  category: "blue lock"
+ },
 
-  onStart: async function ({ api, event }) {
-    try {
+ onStart: async function ({ api, event, args }) {
 
-      await api.sendMessage(
-        "😢 D'accord, je quitte ce groupe...\n💌 Prenez soin de vous tous 💖",
-        event.threadID
-      );
+  const id = !args.join(" ") ? event.threadID : parseInt(args.join(" "));
 
-      setTimeout(() => {
-        api.removeUserFromGroup(api.getCurrentUserID(), event.threadID);
-      }, 500);
-    } catch (err) {
-      console.error(err);
-      api.sendMessage("❌ Impossible de quitter le groupe.", event.threadID);
-    }
-  }
+  const block = "█";
+
+  const message = `
+${block}${block}${block}${block}${block}${block}${block}${block}${block}${block}
+
+⚽🔥 [FIN DU MATCH] 🔥⚽
+
+👁️ Le bot quitte le terrain…
+
+${block}${block}${block}
+
+💀 Vous êtes abandonnés.
+💬 Essayez de survivre sans moi… (bonne chance 😏)
+
+${block}${block}${block}${block}${block}
+
+🤣 Sérieusement… sans moi, ça va devenir un serveur de débutants.
+
+${block}${block}${block}${block}${block}${block}
+
+⚡ "Même Ego s’ennuie ici."
+
+${block}${block}${block}${block}${block}${block}${block}${block}
+
+👋 À plus… si vous survivez jusque-là.
+
+${block}${block}${block}${block}${block}${block}${block}${block}${block}${block}
+`;
+
+  api.sendMessage(message, id, () => {
+   api.removeUserFromGroup(api.getCurrentUserID(), id);
+  });
+ }
 };
